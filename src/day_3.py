9c4c5c6827a83c639e8dc0b9699890ad05cb0d41
Erik van der Goetz <erik.vandergoetz@gmail.com>
2020-01-18 04:39:32 -0500

    day3 inprogress

---
+++ src/day_3.py
@@ -0,0 +1,154 @@
+import operator
+from typing import List, Optional, Union, Tuple
+from collections import namedtuple
+import numbers
+
+
+class Point(object):
+    __slots__ = ['x', 'y']
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other: 'Point'):
+        return self.x == other.x & self.y == other.y
+
+    def __add__(self, other: 'Point'):
+        if not isinstance(other, Point):
+            raise NotImplemented
+        return Point(self.x + other.x, self.y + other.y)
+
+
+    def __sub__(self, other: 'Point'):
+        if not isinstance(other, Point):
+            raise NotImplemented
+        return Point(self.x - other.x, self.y - other.y)
+
+
+    def __mul__(self, other: numbers.Number):
+        # Scalar Multiplication
+        if not isinstance(other, numbers.Number):
+            raise NotImplemented
+        return Point(self.x * other, self.y * other)
+
+    def __rmul__(self, other: numbers.Number):
+        # Scalar Multiplication
+        if not isinstance(other, numbers.Number):
+            raise NotImplemented
+        return Point(self.x * other, self.y * other)
+
+    def __repr__(self) -> str:
+        return(f'{self.__class__.__name__}(x={self.x}, y={self.y})')
+
+    def manhattan_distance(self) -> int:
+        other = Point(0,0)
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
+
+class LineSegment(object):
+    __slots__ = ['point_1', 'point_2', 'orientation']
+
+    def __init__(self, point_1: Point, point_2: Point):
+        self.point_1 = point_1
+        self.point_2 = point_2
+        if point_1.x == point_2.x and point_1.y != point_2.y:
+            self.orientation = 'V'
+        elif point_1.y == point_2.y and point_1.x != point_2.x:
+            self.orientation = 'H'
+
+    def __repr__(self) -> str:
+        #return(f'IntcodeComputer(tape={repr(self._tape)}, head={self._head})')
+        return(f'{self.__class__.__name__}(point_1={repr(self.point_1)}, point_2={repr(self.point_2)})')
+
+    
+    @classmethod
+    def from_manhattan_vector(cls, origin: Point, vector: str):
+        # Vector begins with UDLR and then an integer
+        direction_map = {
+            'U': Point(0,1),
+            'D': Point(0,-1),
+            'R': Point(1,0),
+            'L': Point(-1,0),
+        }
+        if vector[0] not in direction_map.keys() or not vector[1:].isdigit():
+            raise NotImplemented
+        destination = direction_map[vector[0]] * int(vector[1:]) + origin
+        return cls(origin, destination)
+
+
+    def intersects(self, other_linesegment: 'LineSegment') -> Optional[Point]:
+        # If this LineSegment intersects another LineSegment, then return the point that they intersect. Otherwise return None
+        # If line segments are colinear, or start and end on each other we should return None
+        if not isinstance(other_linesegment, LineSegment):
+            raise NotImplementedError
+        # TODO Fix this yucky mess pls
+        if self.orientation == 'H' and other_linesegment.orientation == 'V':
+            if ((self.point_1.x < other_linesegment.point_1.x < self.point_2.x) or \
+            (self.point_1.x > other_linesegment.point_1.x > self.point_2.x)) and \
+            ((other_linesegment.point_1.y < self.point_1.y < other_linesegment.point_2.y) or \
+            (other_linesegment.point_1.y > self.point_1.y > other_linesegment.point_2.y)):
+                return Point(x=other_linesegment.point_1.x, y=self.point_2.y)
+        elif self.orientation == 'V' and other_linesegment.orientation == 'H':
+            if ((self.point_1.y < other_linesegment.point_1.y < self.point_2.y) or \
+            (self.point_1.y > other_linesegment.point_1.y > self.point_2.y)) and \
+            ((other_linesegment.point_1.x < self.point_1.x < other_linesegment.point_2.x) or \
+            (other_linesegment.point_1.x > self.point_1.x > other_linesegment.point_2.x)):
+                return Point(x=self.point_1.x, y=other_linesegment.point_1.y)
+        else:
+            return None
+
+
+
+class Wire(object):
+    __slots__ = ['segments']
+
+    def __init__(self, segments: List[LineSegment]):
+        self.segments = segments
+
+    @classmethod
+    def from_raw_wire_list(cls, raw_wire: List[str]):
+        initial_point = Point(0,0)
+        segments = [LineSegment.from_manhattan_vector(initial_point, raw_wire.pop(0))]
+        for this_segment in raw_wire:
+            segments.append(LineSegment.from_manhattan_vector(segments[-1].point_2, this_segment))
+        return cls(segments)
+
+    def __repr__(self) -> str:
+        return(f'{self.__class__.__name__}({repr(self.segments)})')
+
+    def intersects(self, other_wire: 'Wire') -> List[Point]:
+        #ret = []
+        #for this_segment in self.segments:
+        #    for other_segment in other_wire.segments:
+        #        point = this_segment.intersects(other_segment)
+        #        if point is not None:
+        #            print(f'This segment: {this_segment}, Other Segment: {other_segment}, Intersection: {point}')
+        #            ret.append(point)
+        return [point for point in [this_segment.intersects(other_segment) for this_segment in self.segments for other_segment in other_wire.segments] if point is not None]
+        #return ret
+
+def main():
+    with open('data/day_3.txt', 'r') as day_3_file:
+        raw_red_wire = day_3_file.readline().rstrip('\n').split(',')
+        raw_green_wire = day_3_file.readline().split(',')
+    # Turn each raw wire into a list of coordinate tuples corresponding to each endpoint
+
+    red_wire = Wire.from_raw_wire_list(raw_red_wire)
+    green_wire = Wire.from_raw_wire_list(raw_green_wire)
+
+    intersections = red_wire.intersects(green_wire)
+    sorted_intersections = list(sorted(map(Point.manhattan_distance, intersections)))
+    print(f'Part 1 Answer: {sorted_intersections[0]}')
+
+    #seg_1 = LineSegment(Point(5, 10), Point(8, 10))
+    #seg_2 = LineSegment(Point(6, 8), Point(6, 15))
+    #seg_3 = LineSegment(Point(0, 0), Point(1, 0))
+    #print(seg_1.intersects(seg_2))
+    #print(seg_1.intersects(seg_3))
+
+
+    
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_day_3.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+here = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, here)
+
+from src.day_3 import Point, LineSegment, Wire
+
+
+def test_segment_intersection():
+    segment_1 = LineSegment(point_1=Point(x=3, y=5), point_2=Point(x=3, y=2))
+    segment_2 = LineSegment(point_1=Point(x=6, y=3), point_2=Point(x=2, y=3))
+    intersection_point = segment_1.intersects(segment_2)
+    assert intersection_point == Point(x=3, y=3)
+
+
+def test_wire_intersections():
+    raw_wire_1 = 'R8,U5,L5,D3'
+    wire_1 = Wire.from_raw_wire_list(raw_wire_1.split(','))
+
+    raw_wire_2 = 'U7,R6,D4,L4'
+    wire_2 = Wire.from_raw_wire_list(raw_wire_2.split(','))
+    
+    intersections = wire_1.intersects(wire_2)
+    sorted_intersections = list(sorted(map(Point.manhattan_distance, intersections)))
+
+    assert sorted_intersections[0] == 6
+
+
+def test_wire_intersections_2():
+    raw_wire_1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'
+    wire_1 = Wire.from_raw_wire_list(raw_wire_1.split(','))
+
+    raw_wire_2 = 'U62,R66,U55,R34,D71,R55,D58,R83'
+    wire_2 = Wire.from_raw_wire_list(raw_wire_2.split(','))
+    
+    intersections = wire_1.intersects(wire_2)
+    sorted_intersections = list(sorted(map(Point.manhattan_distance, intersections)))
+
+    assert sorted_intersections[0] == 159
+
+def test_wire_intersections_3():
+    raw_wire_1 = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51'
+    wire_1 = Wire.from_raw_wire_list(raw_wire_1.split(','))
+
+    raw_wire_2 = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
+    wire_2 = Wire.from_raw_wire_list(raw_wire_2.split(','))
+    
+    intersections = wire_1.intersects(wire_2)
+    sorted_intersections = list(sorted(map(Point.manhattan_distance, intersections)))
+
+    assert sorted_intersections[0] == 135
